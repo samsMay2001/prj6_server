3ff8ab0f8d11c53de368f15fb1132b7a8d69c282
Samuel Muhigirwa <127790767+samsMay2001@users.noreply.github.com>
2023-11-11 21:36:17 +0000

    create an express server @15:04

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hello CodeSandbox");
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+
+const PORT = 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.listen(PORT, () => {
+  console.log(`server is live on ${PORT}`);
+});
+console.log("app is running");
