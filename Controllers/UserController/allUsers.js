803dcb58b66faf3692f2f47f4f275912622c5787
Samuel Muhigirwa <127790767+samsMay2001@users.noreply.github.com>
2023-11-26 21:52:05 +0000

    Update allUsers.js

--- Controllers/UserController/allUsers.js
+++ Controllers/UserController/allUsers.js
@@ -27,16 +27,23 @@ const allUsers = async (req, res, User) => {
 
     // this filters out the current user friends
     remaining_users.forEach((user) => {
-      users[currentUserIndex].friends.forEach((friend) => {
-        if (friend.toString() === user._id.toString()) {
-          const newUserObj = { ...user._doc, friend: true };
-          remaining_users1.push(newUserObj);
-        } else {
-          const newUserObj = { ...user._doc, friend: false };
-          remaining_users1.push(newUserObj);
-        }
+      if (users[currentUserIndex].friends.length < 1) {
+        const newUserObj = { ...user._doc, friend: false };
+        remaining_users1.push(newUserObj);
+      } else {
+        users[currentUserIndex].friends.forEach((friend) => {
+          console.log(user);
+          if (friend.toString() === user._id.toString()) {
+            const newUserObj = { ...user._doc, friend: true };
+            remaining_users1.push(newUserObj);
+          } else {
+            const newUserObj = { ...user._doc, friend: false };
+            remaining_users1.push(newUserObj);
+          }
       });
+      }
     });
+    console.log(remaining_users1);
     // this adds the added property for users who have been sent friend request but haven't accepted yet
     const usersWithAddedFlag = remaining_users1.map((userObj) => {
       if (addedUsers.includes(userObj._id.toString())) {
@@ -50,7 +57,6 @@ const allUsers = async (req, res, User) => {
 
     // remove users who are friends already
     // const current_users_1 = usersWithAddedFlag.filter((user) => user.friends.includes())
-
     res.json({
       status: "success",
       data: usersWithAddedFlag,
