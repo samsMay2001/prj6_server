9ad3c92fb8e74a335c8c261528115ebca2687e37
Samuel Muhigirwa <127790767+samsMay2001@users.noreply.github.com>
2023-11-28 00:55:11 +0000

    friend request feature works both ways after many tests and updates on the front end and back end

--- app.js
+++ app.js
@@ -107,8 +107,8 @@ socketIO.on("connection", async (socket) => {
         message: "friend request accepted",
       });
       socketIO.to(to_user.socket_id).emit("request_cancelled", {
-        message: "friend request cancelled"
-      }); 
+        message: "friend request cancelled",
+      });
     } catch (err) {
       console.log(err);
     }
@@ -120,23 +120,21 @@ socketIO.on("connection", async (socket) => {
       const receiver = await User.findById(request_doc.recipient);
 
       // // add to the sender and reciever friends array
-      // sender.friends.push(request_doc.recipient);
-      // receiver.friends.push(request_doc.sender);
+      sender.friends.push(request_doc.recipient);
+      receiver.friends.push(request_doc.sender);
 
-      // await receiver.save({ new: true, validateModifiedOnly: true });
-      // await sender.save({ new: true, validateModifiedOnly: true });
+      await receiver.save({ new: true, validateModifiedOnly: true });
+      await sender.save({ new: true, validateModifiedOnly: true });
 
       // add that a property of accepted to equal to true
-      await FriendRequest.findByIdAndUpdate(
-        data.request_id,
-        { 
-           accepted: true 
-        }
-      );
+      await FriendRequest.findByIdAndUpdate(data.request_id, {
+        accepted: true,
+      });
 
       socketIO.to(sender.socket_id).emit("request_accepted", {
         message: "friend request accepted",
       });
+      // the user may not have a socket id
       socketIO.to(receiver.socket_id).emit("request_accepted", {
         message: "friend request accepted",
       });
