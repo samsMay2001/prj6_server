21c128fe1514b32283da8b7caf253e9ba740370a
admin <muhigirwasamuel2001@gmail.com>
2023-12-06 18:51:02 -0400

    cleaning app.js

--- Controllers/SocketController/textMessage.js
+++ Controllers/SocketController/textMessage.js
@@ -1,2 +1,48 @@
-const onTextMessage = async (data) => {};
+const mongoose = require('mongoose')
+const onTextMessage = async (data, Message, User, ChatRoom, socketIO) => {
+    // data : {from, to, message, type, conversation_id}
+    // Message, User, ChatRoom
+    const {from, to, message, type, created_at, file} = data
+
+    // console.log(to)
+
+    const new_message = {from, to, message, type, created_at, file}
+
+    try{
+      
+      // check if the message already exists
+      const messages = await Message.find({
+        $or: [
+          {
+            from: new mongoose.Types.ObjectId(from),
+            to: new mongoose.Types.ObjectId(to),
+          },
+          {
+            from: new mongoose.Types.ObjectId(to),
+            to: new mongoose.Types.ObjectId(from),
+          },
+        ],
+      });
+      const sender = await User.findById(from)
+      const receiver = await User.findById(to)
+      if (messages.length === 0){
+        // create a new chat
+        const new_chat = {
+          participants : [from, to]
+        }
+        await ChatRoom.create(new_chat)
+      }
+      // add to the messages array
+      await Message.create(new_message)
+      // await onTextMessage(data);
+      socketIO.to(sender.socket_id).emit("message_sent", {
+        message: "message sent successfully"
+      })
+      socketIO.to(receiver.socket_id).emit("new_message", {
+        message: 'new_message'
+      })
+    }catch(err){
+      console.log(err)
+    }
+};
 module.exports = onTextMessage;
--- app.js
+++ app.js
@@ -86,49 +86,7 @@ socketIO.on("connection", async (socket) => {
     await onAcceptRequest(socketIO, data, User, FriendRequest);
   });
   socket.on("text_message", async (data) => {
-    // data : {from, to, message, type, conversation_id}
-    const {from, to, message, type, created_at, file} = data
-
-    // console.log(to)
-
-    const new_message = {from, to, message, type, created_at, file}
-
-    try{
-      
-      // check if the message already exists
-      const messages = await Message.find({
-        $or: [
-          {
-            from: new mongoose.Types.ObjectId(from),
-            to: new mongoose.Types.ObjectId(to),
-          },
-          {
-            from: new mongoose.Types.ObjectId(to),
-            to: new mongoose.Types.ObjectId(from),
-          },
-        ],
-      });
-      const sender = await User.findById(from)
-      const receiver = await User.findById(to)
-      if (messages.length === 0){
-        // create a new chat
-        const new_chat = {
-          participants : [from, to]
-        }
-        await ChatRoom.create(new_chat)
-      }
-      // add to the messages array
-      await Message.create(new_message)
-      // await onTextMessage(data);
-      socketIO.to(sender.socket_id).emit("message_sent", {
-        message: "message sent successfully"
-      })
-      socketIO.to(receiver.socket_id).emit("new_message", {
-        message: 'new_message'
-      })
-    }catch(err){
-      console.log(err)
-    }
+    await onTextMessage(data, Message, User, ChatRoom, socketIO)
     
   });
   socket.on("end", async (data) => {
